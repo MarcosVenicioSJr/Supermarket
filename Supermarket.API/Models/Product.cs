a882f76f64bf0a6bbecbd0aa8cf1e01a6dffdf45
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-07-15 11:04:02 -0300

    feat: create model Product

--- Supermarket.API/Models/Product.cs
+++ Supermarket.API/Models/Product.cs
@@ -1,6 +1,24 @@
-﻿namespace Supermarket.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.API.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Name é obrigatório.")]
+        [MinLength(3, ErrorMessage = "O campo Name deve conter pelo menos 3 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Price é obrigatório.")]
+        [Range((double) 0.05m, (double) decimal.MaxValue, ErrorMessage = "O campo Price deve ser no mínimo 0.5 centavos")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, (double) int.MaxValue, ErrorMessage = "O produto deve conter pelo menos 1 quantidade.")]
+        public int Quantity { get; set; }
     }
 }
