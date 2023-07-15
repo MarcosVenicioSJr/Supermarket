782daea9f4e7a74e6cd63c1331624c8149b238cf
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-07-15 17:14:33 -0300

    feat: Add DataContext and Customer Model

---
+++ Supermarket.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Models;
+
+namespace Supermarket.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Supermarket.API/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Supermarket.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.API.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Name é obrigatório")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public string TaxNumber { get; set; }
+
+        public DateTime LastPurchase { get; set; }
+    }
+}
