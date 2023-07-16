f2a7bb12e8d1ecb38875af7754841a5dcff4dc9a
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-07-16 10:04:10 -0300

    feat: Separating Controller, DTO and Mapper

--- Supermarket.API/Controllers/ProductController.cs
+++ Supermarket.API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Supermarket.API.Data;
+using Supermarket.API.Mapper;
 using Supermarket.API.Models;
 
 namespace Supermarket.API.Controllers
@@ -15,9 +16,19 @@ namespace Supermarket.API.Controllers
         {
             Product? product = await context.Products.FirstOrDefaultAsync(x => x.Id == id);
             if (product == null)
-                return NotFound(new {message = "Produto não encontrado"});
+                return NotFound(new { message = "Produto não encontrado" });
 
             return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody] Product model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            ProductDTO? product = ProductMapper.MapperDtoProduct(model);
+
         }
     }
 }
---
+++ Supermarket.API/Mapper/ProductMapper.cs
@@ -0,0 +1,22 @@
+﻿using Supermarket.API.Models;
+
+namespace Supermarket.API.Mapper
+{
+    public static class ProductMapper
+    {
+        public static ProductDTO MapperDtoProduct(Product model)
+        {
+            ProductDTO product = new ProductDTO()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Price = model.Price,
+                Quantity = model.Quantity,
+                BarCode = model.BarCode,
+                Description = model.Description,
+            };
+
+            return product;
+        }
+    }
+}
